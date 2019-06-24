1e29820862cd555610edab54b729ae158a49c3fd
SBI- <pascal.huerlimann@gmail.com>
2019-06-24 16:07:05 +0200

    Implement adding accessortype and list annotations

--- annotation/src/main/java/ch/sbi/minigit/annotation/XmlListAnnotator.java
+++ annotation/src/main/java/ch/sbi/minigit/annotation/XmlListAnnotator.java
@@ -3,11 +3,18 @@ package ch.sbi.minigit.annotation;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.sun.codemodel.JDefinedClass;
 import com.sun.codemodel.JFieldVar;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import org.jsonschema2pojo.AbstractAnnotator;
 
 public class XmlListAnnotator extends AbstractAnnotator {
+
+  @Override
+  public void propertyInclusion(JDefinedClass clazz, JsonNode schema) {
+    clazz.annotate(XmlAccessorType.class).param("value", XmlAccessType.FIELD);
+  }
 
   @Override
   public void propertyField(
@@ -16,6 +23,7 @@ public class XmlListAnnotator extends AbstractAnnotator {
       field.annotate(XmlElementWrapper.class).param("name", propertyName);
       String name = propertyName.substring(0, propertyName.length() - 1);
       field.annotate(XmlElement.class).param("name", name);
+      field.type().boxify().binaryName();
     }
   }
 }
