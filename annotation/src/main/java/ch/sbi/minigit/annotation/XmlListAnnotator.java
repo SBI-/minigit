0e54bd8780e24517ba32f0096deaa2815f9cef76
SBI- <pascal.huerlimann@gmail.com>
2019-06-24 15:08:31 +0200

    Implement list annotation without explicit typing

--- annotation/src/main/java/ch/sbi/minigit/annotation/XmlListAnnotator.java
+++ annotation/src/main/java/ch/sbi/minigit/annotation/XmlListAnnotator.java
@@ -1,5 +1,21 @@
 package ch.sbi.minigit.annotation;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JFieldVar;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import org.jsonschema2pojo.AbstractAnnotator;
 
-public class XmlListAnnotator extends AbstractAnnotator {}
+public class XmlListAnnotator extends AbstractAnnotator {
+
+  @Override
+  public void propertyField(
+      JFieldVar field, JDefinedClass clazz, String propertyName, JsonNode propertyNode) {
+    if (field.type().fullName().startsWith("java.util.List")) {
+      field.annotate(XmlElementWrapper.class).param("name", propertyName);
+      String name = propertyName.substring(0, propertyName.length() - 1);
+      field.annotate(XmlElement.class).param("name", name);
+    }
+  }
+}
